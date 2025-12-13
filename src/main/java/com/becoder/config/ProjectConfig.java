a66cdae754c640a445f713be875d5f331ec4be52
DurgeshRatnaparkhi <durgeshratnaparkhi536@gmail.com>
2025-12-13 14:20:23 +0530

    Auditing-Category

--- src/main/java/com/becoder/config/ProjectConfig.java
+++ src/main/java/com/becoder/config/ProjectConfig.java
@@ -3,6 +3,9 @@ import org.apache.catalina.mapper.Mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import com.becoder.audit.AuditAwareConfig;
 
 @Configuration
 public class ProjectConfig {
@@ -12,4 +15,13 @@ public class ProjectConfig {
     	
         return new ModelMapper();
     }
+    
+    
+    
+    @Bean
+	public AuditorAware<Integer> auditAware() {
+    	
+		return new AuditAwareConfig();
+	}
+
 }
